574c32dcf8e642816c1019f9e05918202f2e0524
Anubhab Mukherjee <anubhab.mukherjee2013@gmail.com>
2019-06-23 20:19:16 +0530

    Added basic library implementation.

---
+++ ReSourcer_Lib/resourcer.cpp
@@ -0,0 +1,11 @@
+#include "resourcer.hpp"
+
+ReSourcer::ReSourcer()
+{
+}
+
+std::vector<unsigned> ReSourcer::getItem(std::string item)
+{
+  std::vector<unsigned> __RCC__data__store__(std::string id);
+  return __RCC__data__store__(item);
+}
---
+++ ReSourcer_Lib/resourcer.hpp
@@ -0,0 +1,15 @@
+#ifndef RESOURCER_HPP
+#define RESOURCER_HPP
+
+#include <vector>
+#include <string>
+
+class ReSourcer
+{
+
+public:
+  ReSourcer();
+  static std::vector<unsigned> getItem(std::string);
+};
+
+#endif // RESOURCER_HPP
